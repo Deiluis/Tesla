b187c2926399c5df89322a17e9367cf3682dc9e9
Thomi <chavez.thomas970@gmail.com>
2023-08-22 11:08:30 -0300

    Exposicion (Node.js)

--- includes/ver-exposicion.php
+++ includes/ver-exposicion.php
@@ -1,3 +1,12 @@
 <div class="content-wrapper" id="ver-exposicion">
     <h2>Ver exposicion</h2>
+    <img id="play">
 </div>
+<script type="text/javascript" src='http://181.45.18.185:7777/socket.io/socket.io.js'></script>
+<script>
+    const socket = io();
+    const img = document.getElementById('play');
+    socket.on('stream', (image) => {
+        img.src = image;
+    })
+</script>
