bdc819724aae7aa0bffc84f6ab62eb24ee1fa838
Thomi <chavez.thomas970@gmail.com>
2023-08-22 19:34:17 -0300

    Exposicion (Node.js)

--- includes/exponer.php
+++ includes/exponer.php
@@ -1,5 +1,55 @@
 <div class="content-wrapper" id="exposicion">
-    <div class="content-section">
-        <h2>Exponer</h2>
+    <div id="controls"></div>
+    <div id="emitir" style="display: none">
+        <button onclick="emitir()" style="margin-bottom: 5px">Emitir</button>
+        <video id="video" style="width: 100%; height: 100%" autoplay="true"></video>
+        <canvas id="preview" style="display:none"></canvas>
     </div>
+    <script>
+        const socket = io("http://localhost:7777");
+        const controls = document.querySelector('#controls');
+        const video = document.getElementById('video');
+        const canvas = document.getElementById('preview');
+        const context = canvas.getContext('2d');
+        canvas.width = 1600;
+        canvas.height = canvas.width * 0.5;
+        context.width = canvas.width;
+        context.height = canvas.height;
+
+        socket.on("connect_error", (error) => {
+            document.querySelector('h2').innerHTML = "Error en conexion";
+        });
+        let roomId;
+        function joinRoom(){
+            if (document.querySelector('#room').value === '') {
+                return alert('Please type a room ID');
+            }
+            roomId = document.querySelector('#room').value
+            controls.innerHTML += ``;
+            controls.style.display = `none`;
+            document.querySelector('#emitir').style.display = `block`;
+            return socket.emit('room', roomId);
+        }
+
+        function loadCam(stream) {
+            video.srcObject = stream
+        }
+
+        function emitir(){
+            navigator.mediaDevices.getDisplayMedia({video: {cursor: "always"}, audio: false}).then(loadCam).catch(() => {
+                console.log('errors with the media device')
+            })
+            const intervalo = setInterval(() => {
+                context.drawImage(video, 0, 0, context.width, context.height);
+                socket.emit('stream', { video: canvas.toDataURL('image/webp'), roomId});
+            }, 1000)
+        }
+        socket.on("connect", () => {
+            controls.innerHTML = `
+                <input id="room" type="number">
+                <button type="submit" onClick=joinRoom()>Entrar a la sala</button>
+            `;
+        });
+
+    </script>
 </div>
--- includes/ver-exposicion.php
+++ includes/ver-exposicion.php
@@ -1,11 +1,33 @@
 <div class="content-wrapper" id="ver-exposicion">
-    <!-- <h2>Ver exposición</h2> -->
-    <img id="play">
+    <h2></h2>
+    <div id="controls"></div>
+    <img id="play" style="display:none">
     <script>
         const socket = io("http://localhost:7777");
-        const img = document.getElementById('play');
-        socket.on('stream', (image) => {
-            img.src = image;
-        })
+        const controls = document.querySelector('#controls');
+        socket.on("connect_error", (error) => {
+            document.querySelector('h2').innerHTML = "Error en conexion";
+        });
+        let roomId;
+        function joinRoom(){
+            if (document.querySelector('#room').value === '') {
+                return alert('Please type a room ID');
+            }
+            roomId = document.querySelector('#room').value
+            controls.innerHTML = ``;
+            controls.style.display = `none`;
+            return socket.emit('room', roomId);
+        }
+        socket.on("connect", () => {
+            controls.innerHTML = `
+                <input id="room" type="number">
+                <button type="submit" onClick=joinRoom()>Entrar a la sala</button>
+            `;
+            const img = document.getElementById('play');
+            img.style.display = 'block';
+            socket.on("stream", (image) => {
+                img.src = image;
+            })
+        });
     </script>
 </div>
