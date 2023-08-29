746e9cd035db9d8256fff6040f12fa6f6460bee2
zhren-byte <chavez.thomas970@gmail.com>
2023-08-28 22:42:14 -0300

    Errores menores

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
+const port = process.env.PORT || 7777
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
   }
 });
-app.set("view engine", "hbs");
-app.set("views", __dirname + "\\views");
-app.use('/', express.static('public'))
-app.get('/profesor', function(req, res, next) {
-  res.render('profesor');
-});
+
 io.on('connection', (socket) => {
   socket.on('room', (roomId) => {
     socket.join(roomId); //Une al socket a la sala
   })
   socket.on('stream', (video) => {
-    socket.to(video.roomId).emit('stream', video.stream); //Emitir el evento a todos los sockets conectados
+    socket.to(video.roomId).emit('stream', video.stream); //Emitir el evento a todos los sockets conectados a la sala
   })
 })
 
-// INICIO DEL SERVIDOR =================================================================
-const port = process.env.PORT || 7777
 server.listen(port, () => {
   console.log(`Express server listening on port ${port}`)
 })
